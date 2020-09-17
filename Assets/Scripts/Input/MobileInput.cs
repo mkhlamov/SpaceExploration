898c757c265b1d6ba7bde3bc72648e9170d98b51
mkhlamov <mkhlamov@gmail.com>
2020-09-17 22:39:12 +0300

    Add mobile pitch zoom

--- Assets/Scripts/Input/MobileInput.cs
+++ Assets/Scripts/Input/MobileInput.cs
@@ -1,31 +1,36 @@
-﻿using SpaceExploration.Interfaces;
+﻿using System;
+using SpaceExploration.Interfaces;
 using UnityEngine;
 
 namespace SpaceExploration.Input
 {
     public class MobileInput : IInputService
     {
-        private Vector2 touchOrigin = -Vector2.one;
+        private Vector2 _touchOrigin = -Vector2.one;
+        
+        private Vector2 _touchZoomZeroOrigin = -Vector2.one;
+        private Vector2 _touchZoomOneOrigin = -Vector2.one;
+
         public Vector2 GetMovementInputDelta()
         {
             var movement = Vector2.zero;
-            if (UnityEngine.Input.touchCount > 0)
+            if (UnityEngine.Input.touchCount == 1)
             {
                 var touch = UnityEngine.Input.touches[0];
 
                 switch (touch.phase)
                 {
                     case TouchPhase.Began:
-                        touchOrigin = touch.position;
+                        _touchOrigin = touch.position;
                         break;
-                    case TouchPhase.Ended when touchOrigin.x >= 0:
+                    case TouchPhase.Ended when _touchOrigin.x >= 0:
                     {
                         var touchEnd = touch.position;
-                        var x = (touchEnd.x - touchOrigin.x) / Screen.width;
-                        var y = (touchEnd.y - touchOrigin.y) / Screen.height;
+                        var x = (touchEnd.x - _touchOrigin.x) / Screen.width;
+                        var y = (touchEnd.y - _touchOrigin.y) / Screen.height;
                         movement = new Vector2(Mathf.Abs(x) > 0.1f ? x : 0f,
                             Mathf.Abs(y) > 0.1f ? y : 0f);
-                        touchOrigin.x = -1;
+                        _touchOrigin.x = -1;
                         break;
                     }
                 }
@@ -36,7 +41,36 @@ namespace SpaceExploration.Input
 
         public float GetZoomDelta()
         {
-            return 0;
+            var zoomDelta = 0f;
+            if (UnityEngine.Input.touchCount == 2)
+            {
+                var touchZero = UnityEngine.Input.GetTouch(0);
+                var touchOne = UnityEngine.Input.GetTouch(1);
+
+                switch (touchZero.phase)
+                {
+                    case TouchPhase.Began:
+                        _touchZoomZeroOrigin = touchZero.position;
+                        _touchZoomOneOrigin = touchOne.position;
+                        break;
+                    case TouchPhase.Ended:
+                        var prevMagnitude = (_touchZoomZeroOrigin - _touchZoomOneOrigin).magnitude;
+                        var currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+                        zoomDelta = currentMagnitude - prevMagnitude;
+                        break;
+                    case TouchPhase.Moved:
+                        break;
+                    case TouchPhase.Stationary:
+                        break;
+                    case TouchPhase.Canceled:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            return zoomDelta / 100f;
         }
     }
 }
